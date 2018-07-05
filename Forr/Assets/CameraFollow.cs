9fad706148c9a8015ad7129c1c53e03365ff9051
Anumey Sharma <anumeysharma_shashidhar@srmuniv.edu.in>
2018-07-05 16:18:52 +0530

    Forest Adventure Upload

---
+++ Forr/Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+        mycam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mycam.orthographicSize = (Screen.height / 100f) / 4f;
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0, 0, -10);
+        }
+
+	}
+}
---
+++ Forr/Assets/Warp_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp_Script : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("An object has collided");
+        other.gameObject.transform.position = warpTarget.position;
+        Camera.main.transform.position = warpTarget.position;
+    }
+
+}
